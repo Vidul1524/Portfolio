1408a3ad2d8ccf8dd1ad180f876f480e8e21e290
Vidul Garg <141549841+Vidul1524@users.noreply.github.com>
2024-06-28 19:36:31 +0530

    Update Footer.js

--- reactPortfolioWebsite/src/components/footer/Footer.js
+++ reactPortfolioWebsite/src/components/footer/Footer.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import './footer.css';
 import { FaLinkedinIn } from "react-icons/fa6";
 import { IoLogoInstagram } from "react-icons/io";
 
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>VidulGarg</a>
+      <a href="#home" className='footer__logo'>VidulGarg</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
@@ -18,15 +18,15 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-        <a href="https://www.linkedin.com/in/vidul-garg-vg152410/"><FaLinkedinIn/></a>
-        <a href="https://www.instagram.com/v._dul._/"><IoLogoInstagram/></a>
+        <a href="https://www.linkedin.com/in/vidul-garg-vg152410/"><FaLinkedinIn /></a>
+        <a href="https://www.instagram.com/v._dul._/"><IoLogoInstagram /></a>
       </div>
 
       <div className="footer__copyright">
         <small>&copy;Vidul Garg. All rights reserved</small>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
